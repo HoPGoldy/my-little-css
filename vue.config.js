d24ba77730246f49cf246bdc5523c51217df8c3b
huang puguang <puguang_huang@fpi-inc.com>
2020-06-10 09:40:37 +0800

    feat: 更新预览

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const resolve = require('path').resolve
+const appName = require('./package.json').name
 
 // vue.config.js
 module.exports = {
@@ -17,29 +18,32 @@ module.exports = {
                 .plugin('webpack-bundle-analyzer')
                 .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
         }
-
-        config
-            .plugin('html-webpack-plugin')
-            .use(require('html-webpack-plugin'), [{
-                publicPath: `/${require('./package.json').name}/`,
-            }])
-        config
-            .plugin('webpack-cdn-plugin')
-            .use(require('webpack-cdn-plugin'), [{
-                modules: [
-                    {
-                        name: 'vue',
-                        var: 'Vue',
-                        path: 'vue.min.js'
-                    },
-                    {
-                        name: 'vue-router',
-                        var: 'VueRouter',
-                        path: 'vue-router.min.js'
-                    }
-                ],
-                prodUrl: '//cdn.bootcdn.net/ajax/libs/:name/:version/:path'
-            }])
+        
+        // config
+        //     .plugin('html-webpack-plugin')
+        //     .use(require('html-webpack-plugin'), [{
+        //         publicPath: './',
+        //         title: appName,
+        //         favicon: resolve(__dirname, './public/favicon.ico'),
+        //         template: resolve(__dirname, './public/index.html'),
+        //     }])
+        // config
+        //     .plugin('webpack-cdn-plugin')
+        //     .use(require('webpack-cdn-plugin'), [{
+        //         modules: [
+        //             {
+        //                 name: 'vue',
+        //                 var: 'Vue',
+        //                 path: 'vue.min.js'
+        //             },
+        //             {
+        //                 name: 'vue-router',
+        //                 var: 'VueRouter',
+        //                 path: 'vue-router.min.js'
+        //             }
+        //         ],
+        //         prodUrl: 'https://cdn.bootcdn.net/ajax/libs/:name/:version/:path',
+        //     }])
 
         const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
         types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
